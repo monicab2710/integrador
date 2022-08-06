789401ce09a4c1b892abe33a6cf878509bbf4fe9
MónicaBernal <93306885+monicab2710@users.noreply.github.com>
2022-08-06 16:03:58 -0500

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,16 @@
-
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Header from "./components/Header/Header";
-import Register from "./pages/Register";
-import LogIn from "./pages/LogIn";
+import React from 'react';
+import Footer from './components/Footer/Footer'
+import Body from "./components/Body/Body"
 
 function App() {
   return (
-    <Router>
-      
-      <Routes>
-      <Route path="/" element={<Header />}>
-      <Route
-        path="/register" element={<Register/>}/>
-      
-      <Route path="/logIn" element={<LogIn />} />
-      </Route>
-     
-        
-      </Routes>
-    </Router>
+    <>
+      <div className="App">
+        <Body />
+       <Footer/>
+      </div>
+
+    </>
   );
 }
 
